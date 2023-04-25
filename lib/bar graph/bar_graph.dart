c75ad6825611fb1ffa756f30cf195a449ff301cc
sumitpant190 <114337820+sumitpant190@users.noreply.github.com>
2023-04-25 13:15:58 +0545

    decoration

--- lib/bar graph/bar_graph.dart
+++ lib/bar graph/bar_graph.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:bar_graph/bar%20graph/bar_data.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class MyBarGraph extends StatelessWidget {
   final List weeklySummary; //[sunAmount, monAmount,  ...., satAmount ]
@@ -26,14 +26,36 @@ class MyBarGraph extends StatelessWidget {
     myBarData.initializeBarData();
     return BarChart(
       BarChartData(
-        maxY: 200,
+        maxY: 100,
         minY: 0,
+        gridData: FlGridData(show: false),
+        borderData: FlBorderData(show: false),
+        titlesData: FlTitlesData(
+          show: true,
+          topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          bottomTitles: AxisTitles(
+            sideTitles:
+                SideTitles(showTitles: true, getTitlesWidget: getBottomTitles),
+          ),
+        ),
         barGroups: myBarData.barData
             .map(
               (data) => BarChartGroupData(
                 x: data.x,
                 barRods: [
-                  BarChartRodData(toY: data.y),
+                  BarChartRodData(
+                    toY: data.y,
+                    color: Colors.grey[800],
+                    width: 20,
+                    borderRadius: BorderRadius.circular(4),
+                    backDrawRodData: BackgroundBarChartRodData(
+                      show: true,
+                      toY: 100,
+                      color: Colors.grey[200],
+                    ),
+                  ),
                 ],
               ),
             )
@@ -41,4 +63,62 @@ class MyBarGraph extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget getBottomTitles(double value, TitleMeta meta) {
+  const style =
+      TextStyle(color: Colors.grey, fontWeight: FontWeight.bold, fontSize: 14);
+
+  Widget text;
+  switch (value.toInt()) {
+    case 0:
+      text = const Text(
+        'S',
+        style: style,
+      );
+      break;
+    case 1:
+      text = const Text(
+        'M',
+        style: style,
+      );
+      break;
+    case 2:
+      text = const Text(
+        'T',
+        style: style,
+      );
+      break;
+    case 3:
+      text = const Text(
+        'W',
+        style: style,
+      );
+      break;
+    case 4:
+      text = const Text(
+        'Th',
+        style: style,
+      );
+      break;
+    case 5:
+      text = const Text(
+        'Fr',
+        style: style,
+      );
+      break;
+    case 6:
+      text = const Text(
+        'Sa',
+        style: style,
+      );
+      break;
+    default:
+      text = const Text(
+        '',
+        style: style,
+      );
+      break;
+  }
+  return SideTitleWidget(child: text, axisSide: meta.axisSide);
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -32,9 +32,10 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       body: Center(
         child: SizedBox(
-          height: 400,
+          height: 200,
           child: MyBarGraph(
             weeklySummary: weeklySummary,
           ),
